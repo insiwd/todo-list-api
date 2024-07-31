3fb83e2c3e7bd4a9ec9adaee1c1b8d122e1926b7
insiwd <ruan.cavalcante505@gmail.com>
2024-07-31 08:32:34 -0300

    feat: attributes + getter/setter

--- src/main/java/com/example/todo_list/entity/Todo.java
+++ src/main/java/com/example/todo_list/entity/Todo.java
@@ -1,10 +1,53 @@
 package com.example.todo_list.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "todos")
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String nome;
+    private String descricao;
+    private boolean completado;
+    private int prioridade;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public boolean isCompletado() {
+        return completado;
+    }
+    public void setCompletado(boolean completado) {
+        this.completado = completado;
+    }
+    public int getPrioridade() {
+        return prioridade;
+    }
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    
 }
